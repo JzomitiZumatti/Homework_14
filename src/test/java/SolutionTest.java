b9cc051e7fddd72e87f65688106b5b853e73d839
JzomitiZumatti <evgeniyshinkar@gmail.com>
2023-11-26 16:16:47 +0200

    Added three methods which solves three problems. Also added one positive test for each method.

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,3 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
 public class SolutionTest {
 
+    @Test
+    public void numJewelsInStonesTest() {
+        Solution solution = new Solution();
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+        int expected = 3;
+
+        int result = solution.numJewelsInStones(jewels, stones);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void maxNumberOfBalloons() {
+        Solution solution = new Solution();
+        String text = "loonbalxballpoon";
+        int expected = 2;
+
+        int result = solution.maxNumberOfBalloons(text);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void numIdenticalPairs() {
+        Solution solution = new Solution();
+        int[] numbers = {1,2,3,1,1,3};
+        int expected = 4;
+
+        int result = solution.numIdenticalPairs(numbers);
+
+        Assert.assertEquals(expected, result);
+    }
 }
